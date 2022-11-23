7aa1196478eb347a1f958e760b7de477be12ca89
Jean Cruz <jeansc@outlook.com>
2022-11-22 21:02:57 -0300

    lançamento de exceções

--- bytebank-herdado/src/javaPilha/FluxoComTratamento.java
+++ bytebank-herdado/src/javaPilha/FluxoComTratamento.java
@@ -1,6 +1,6 @@
 package javaPilha;
 
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -23,9 +23,13 @@ public class Fluxo {
     private static void metodo2() {
 
         System.out.println("Ini do metodo2");
-        throw new ArithmeticException("deu errado");
-
-        //System.out.println("Fim do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            int a = i / 0;
+            //Vazio c = null;
+            //c.emBranco();
+        }
+        System.out.println("Fim do metodo2");
 
     }
 }
