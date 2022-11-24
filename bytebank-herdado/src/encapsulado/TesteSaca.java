e894e877c5db58465624e67d56c959c7f7cba66f
Jean Cruz <jeansc@outlook.com>
2022-11-23 21:17:49 -0300

    aplicando exceções

--- bytebank-herdado/src/encapsulado/TesteSaca.java
+++ bytebank-herdado/src/encapsulado/TesteSaca.java
@@ -4,7 +4,12 @@ public class TesteSaca {
     public static void main(String[] args) {
         Conta conta = new ContaCorrente(123,321);
         conta.deposita(200.0);
-        conta.saca(210.0);
+        try {
+            conta.saca(210.0);
+        }catch (SaldoInsuficienteException ex){
+            System.out.println("Ex: "+ex.getMessage());
+        }
+
 
         System.out.println(conta.getSaldo());
     }
