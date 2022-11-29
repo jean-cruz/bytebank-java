f2dde6dc261bbfdd32e9f7fda9d8edfe2f665856
Jean Cruz <jeansc@outlook.com>
2022-11-29 20:44:18 -0300

    jar

---
+++ bytebank-herdado/src/br/com/bytebank/TesteBiblioteca.java
@@ -0,0 +1,10 @@
+package br.com.bytebank;
+
+import br.com.bytebank.banco.modelo.*;
+
+public class TesteBiblioteca {
+    public static void main(String[] args) {
+        Conta c = new ContaCorrente(123,321);
+
+    }
+}
