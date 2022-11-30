734b01b8b6ad2bc3a2e5d565b94bf675571cb94b
Jean Cruz <jeansc@outlook.com>
2022-11-30 19:58:44 -0300

    interface charSequence

--- bytebank-herdado/src/br/com/bytebank/banco/test/TesteString.java
+++ bytebank-herdado/src/br/com/bytebank/banco/test/TesteString.java
@@ -35,5 +35,28 @@ public class TesteString {
         System.out.println("isEmpty espaço:"+" ".isEmpty());
         System.out.println("Trim :"+" Jean Vinicius Santos da Cruz ".trim());
         System.out.println("contains :"+nome.contains("Alu"));
+
+        String texto = "Socorram";
+        texto = texto.concat("-");
+        texto = texto.concat("me");
+        texto = texto.concat(", ");
+        texto = texto.concat("subi ");
+        texto = texto.concat("no ");
+        texto = texto.concat("ônibus ");
+        texto = texto.concat("em ");
+        texto = texto.concat("Marrocos");
+        System.out.println(texto);
+
+        StringBuilder builder = new StringBuilder("Socorram");
+        builder.append("-");
+        builder.append("me");
+        builder.append(", ");
+        builder.append("subi ");
+        builder.append("no ");
+        builder.append("ônibus ");
+        builder.append("em ");
+        builder.append("Marrocos");
+        String texto2 = builder.toString();
+        System.out.println(texto2);
     }
 }
