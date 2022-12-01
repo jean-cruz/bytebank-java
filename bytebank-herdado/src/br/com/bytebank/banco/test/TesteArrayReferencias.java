1fcdb6daec5b8973adc2a3f2d8f7828f6bb85182
Jean Cruz <jeansc@outlook.com>
2022-12-01 20:49:10 -0300

    Arrays

--- bytebank-herdado/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ bytebank-herdado/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,22 +1,29 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+        //Conta[] contas = new Conta[5];
+        Object[] referencias = new Object[5];
 
         ContaCorrente cc1 = new ContaCorrente(22,11);
+        referencias[0] = cc1;
 
-        contas[0] = cc1;
+        ContaPoupanca cc2 = new ContaPoupanca(22,22);
+        referencias[1] = cc2;
 
-        ContaCorrente cc2 = new ContaCorrente(22,22);
+        referencias[3] = new Cliente();
+        Cliente cliente = (Cliente) referencias[3];
 
-        contas[1] = cc2;
-        System.out.println("contas[0] = "+contas[3].getNumero());
-        System.out.println("contas[1] = "+contas[1].getNumero());
 
-        ContaCorrente ref = contas[1];
+        //System.out.println("contas[0] = "+contas[0].getNumero());
+        //System.out.println("contas[1] = "+contas[1].getNumero());
+
+        ContaPoupanca ref = (ContaPoupanca) referencias[1];
         System.out.println("ref = "+ref.getNumero());
         System.out.println("cc2 = "+cc2.getNumero());
     }
