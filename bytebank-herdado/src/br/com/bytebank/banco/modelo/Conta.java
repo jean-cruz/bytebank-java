6aab8cbe69fcaefaceca638b9200a1564b562508
Jean Cruz <jeansc@outlook.com>
2022-12-06 20:46:00 -0300

    Ordenação de listas

--- bytebank-herdado/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado/src/br/com/bytebank/banco/modelo/Conta.java
@@ -5,7 +5,7 @@ package br.com.bytebank.banco.modelo;
  *
  * @author jean.cruz
  */
-public abstract class Conta {
+public abstract class Conta implements Comparable<Conta> {
     @DoublePositivo
     double saldo;
     private int agencia;
@@ -89,7 +89,12 @@ public abstract class Conta {
     }
     @Override
     public String toString() {
-        return "Numero:"+this.getNumero()+", Agencia:"+this.getAgencia()+", Saldo:"+this.getSaldo();
+        return "Numero:" + this.getNumero() + ", Agencia:" + this.getAgencia() + ", Saldo:" + this.getSaldo();
+    }
+
+    @Override
+    public int compareTo(Conta outra) {
+        return Double.compare(this.saldo,outra.saldo);
     }
 
     @Override
--- bytebank-herdado/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado/src/br/com/bytebank/banco/test/util/Teste.java
@@ -3,22 +3,36 @@ package br.com.bytebank.banco.test.util;
 import br.com.bytebank.banco.modelo.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class Teste {
     public static void main(String[] args) {
         Conta cc1 = new ContaCorrente(22, 33);
+        Cliente clienteCC1 = new Cliente();
+        clienteCC1.setNome("Nico");
+        cc1.setTitular(clienteCC1);
         cc1.deposita(333.0);
 
         Conta cc2 = new ContaPoupanca(22, 44);
+        Cliente clienteCC2 = new Cliente();
+        clienteCC2.setNome("Guilherme");
+        cc2.setTitular(clienteCC2);
         cc2.deposita(444.0);
 
         Conta cc3 = new ContaCorrente(22, 11);
+        Cliente clienteCC3 = new Cliente();
+        clienteCC3.setNome("Paulo");
+        cc3.setTitular(clienteCC3);
         cc3.deposita(111.0);
 
         Conta cc4 = new ContaPoupanca(22, 22);
+        Cliente clienteCC4 = new Cliente();
+        clienteCC4.setNome("Ana");
+        cc4.setTitular(clienteCC4);
         cc4.deposita(222.0);
+
 
         List<Conta> lista = new ArrayList<>();
         lista.add(cc1);
@@ -26,21 +40,43 @@ public class Teste {
         lista.add(cc3);
         lista.add(cc4);
 
-        for (Conta conta:lista) {
+        for (Conta conta : lista) {
             System.out.println(conta);
         }
 
-        lista.sort(new NumeroDaContaComparator());
-        System.out.println("------------------------------------------------------");
-        for (Conta conta:lista) {
-            System.out.println(conta);
+        //NumeroDaContaComparator comparator = new NumeroDaContaComparator();
+        //TitularDaContaComparator titularComparator = new TitularDaContaComparator();
+        //lista.sort(new TitularDaContaComparator());
+        lista.sort(null);
+        //Collections.sort(lista,new NumeroDaContaComparator());
+        //Collections.sort(lista);
+        //Collections.reverse(lista);
+
+        System.out.println("---------");
+
+        for (Conta conta : lista) {
+            System.out.println(conta + ", "+conta.getTitular().getNome());
         }
+
+    }
+}
+class TitularDaContaComparator implements Comparator<Conta>{
+    @Override
+    public int compare(Conta conta1, Conta conta2) {
+        String nome1 = conta1.getTitular().getNome();
+        String nome2 = conta2.getTitular().getNome();
+
+        return nome1.compareTo(nome2);
     }
 }
 class NumeroDaContaComparator implements Comparator<Conta> {
 
     @Override
     public int compare(Conta conta1, Conta conta2) {
+
+        //return conta1.getNumero() - conta2.getNumero();
+        return Integer.compare(conta1.getNumero(),conta2.getNumero());
+        /*
         if(conta1.getNumero() < conta2.getNumero()){
             return -1;
         }
@@ -48,5 +84,6 @@ class NumeroDaContaComparator implements Comparator<Conta> {
             return 1;
         }
         return 0;
+        */
     }
 }
