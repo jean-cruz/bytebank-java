aea8c1ef2cb50a806425a8b88d4988ba25285880
Jean Cruz <jeansc@outlook.com>
2022-12-07 20:34:50 -0300

    java io read file

--- bytebank-herdado/src/javaIO/TesteLeitura.java
+++ bytebank-herdado/src/javaIO/TesteLeitura.java
@@ -1,21 +1,21 @@
 package javaIO;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class TesteLeitura {
     public static void main(String[] args) throws IOException {
         //fluxo de entrada com arquivo
-        FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis);
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
         String linha = br.readLine();
 
-        System.out.println(linha);
+        while(linha != null){
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
         br.close();
     }
-
 }
