471beae1ccc88477511873fa9bd9df7e219048a7
Jean Cruz <jeansc@outlook.com>
2022-12-07 21:04:32 -0300

    java io input and output

--- bytebank-herdado/src/javaIO/TesteCopiarArquivo.java
+++ bytebank-herdado/src/javaIO/TesteCopiarArquivo.java
@@ -1,23 +1,32 @@
 package javaIO;
 
 import java.io.*;
+import java.net.Socket;
 
 public class TesteCopiarArquivo {
     public static void main(String[] args) throws IOException {
+
+        //Socket s = new Socket();
+
         //fluxo de entrada com arquivo
         InputStream fis = new FileInputStream("lorem.txt");
+        //InputStream fis = System.in;
+        //InputStream fis = s.getInputStream();
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
         OutputStream fos = new FileOutputStream("lorem2.txt");
+        //OutputStream fos = System.out;
+        //OutputStream fos = s.getOutputStream();
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
 
         String linha = br.readLine();
 
-        while(linha != null){
+        while(linha != null && !linha.isEmpty()){
             bw.write(linha);
             bw.newLine();
+            bw.flush();
             linha = br.readLine();
         }
 
