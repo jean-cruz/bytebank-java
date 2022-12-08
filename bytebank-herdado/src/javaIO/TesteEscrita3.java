19d5f80c1d119ba3d7aae9eb733455c1cf2481b1
Jean Cruz <jeansc@outlook.com>
2022-12-07 21:28:28 -0300

    PrintStream

--- bytebank-herdado/src/javaIO/TesteEscrita3.java
+++ bytebank-herdado/src/javaIO/TesteEscrita3.java
@@ -10,12 +10,13 @@ public class TesteEscrita3 {
         //BufferedWriter bw = new BufferedWriter(osw);
 
         //BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
-        PrintStream ps = new PrintStream(new File("lorem2.txt"));
+        //PrintStream ps = new PrintStream(new File("lorem2.txt"));
+        PrintWriter ps = new PrintWriter("lorem2.txt");
         ps.println("during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.");
         ps.println();
         ps.println();
         ps.println();
-        ps.println("dasdasdasdasd dladsadjçasdka daçldas teste escrita 3");
+        ps.println("dasdasdasdasd dladsadjçasdka daçldas teste escrita 3"+ps.getClass().getName());
         ps.close();
     }
 }
