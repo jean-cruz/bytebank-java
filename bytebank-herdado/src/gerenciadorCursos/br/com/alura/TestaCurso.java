d7d9c9d231f90cd6db0845499fa68cd42a24f443
Jean Cruz <jeansc@outlook.com>
2022-12-12 20:37:01 -0300

    relacionamentos com coleções

---
+++ bytebank-herdado/src/gerenciadorCursos/br/com/alura/TestaCurso.java
@@ -0,0 +1,19 @@
+package gerenciadorCursos.br.com.alura;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando as coleções do Java","Paulo Silveira");
+
+        //javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList",21));
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList",21));
+        javaColecoes.adiciona(new Aula("Criando uma Aula",20));
+        javaColecoes.adiciona(new Aula("Modelando comcoleções",22));
+
+        List<Aula> aulas = javaColecoes.getAulas();
+
+        System.out.println(javaColecoes.getAulas());
+    }
+}
