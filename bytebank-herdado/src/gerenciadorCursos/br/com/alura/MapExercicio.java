6b866ab261e1dc7b1d87e2d3e5944b18cc28c3f1
Jean Cruz <jeansc@outlook.com>
2022-12-14 20:49:56 -0300

    map entry

--- bytebank-herdado/src/gerenciadorCursos/br/com/alura/MapExercicio.java
+++ bytebank-herdado/src/gerenciadorCursos/br/com/alura/MapExercicio.java
@@ -1,7 +1,9 @@
 package gerenciadorCursos.br.com.alura;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapExercicio {
     public static void main(String[] args) {
@@ -14,5 +16,18 @@ public class MapExercicio {
 
         pessoas.keySet().forEach(idade -> System.out.println(pessoas.get(idade)));
 
+        Set<Integer> chaves = pessoas.keySet();
+        for (Integer idade : chaves) {
+            System.out.println(idade);
+        }
+        Collection<String> valores = pessoas.values();
+        for (String nomes : valores) {
+            System.out.println(nomes);
+        }
+
+        Set<Map.Entry<Integer,String>> associacoes = pessoas.entrySet();
+        for (Map.Entry<Integer,String> associacao : associacoes) {
+            System.out.println(associacao.getKey() + " - " + associacao.getValue());
+        }
     }
 }
